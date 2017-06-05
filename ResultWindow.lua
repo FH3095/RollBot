40bead674426b7c99fe243a296ab3c1bc111a47f
FH3095 <FH3095@users.noreply.github.com>
2017-06-05 18:50:14 +0200

    Add example code for window

--- ResultWindow.lua
+++ ResultWindow.lua
@@ -5,6 +5,20 @@ local log = RollBotDebug.log
 function RB:openResultWindow()
 	-- TODO implement
 	log("OpenResultWindow")
+	local AceGUI = LibStub("AceGUI-3.0")
+	-- Create a container frame
+	local f = AceGUI:Create("Frame")
+	f:SetCallback("OnClose",function(widget) AceGUI:Release(widget) end)
+	f:SetTitle("AceGUI-3.0 Example")
+	f:SetStatusText("Status Bar")
+	f:SetLayout("Flow")
+	-- Create a button
+	local btn = AceGUI:Create("Button")
+	btn:SetWidth(170)
+	btn:SetText("Button !")
+	btn:SetCallback("OnClick", function() print("Click!") end)
+	-- Add the button to the container
+	f:AddChild(btn)
 end
 
 function RB:resultAddRoll(name, roll, rollMin, rollMax)
@@ -13,5 +27,5 @@ function RB:resultAddRoll(name, roll, rollMin, rollMax)
 end
 
 function RB:resultClearRolls()
-	-- TODO implement
+-- TODO implement
 end
